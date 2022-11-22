2c0be66cedfadc4b223f20a03447f8ac43b9fe2d
simca09 <delacruz.klydean@yahoo.com>
2022-11-21 21:47:31 -0500

    Buttons added in

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+const grid = document.createElement("div");
+grid.classList.add("grid");
+grid.textContent = "whattttt";
+
+container.appendChild(grid);
