627280181ca9faad8c93527334fd39f416032ca3
simca09 <delacruz.klydean@yahoo.com>
2022-11-22 21:06:48 -0500

    Grid size fixed

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
-const container = document.querySelector(".container");
+// const board = document.getElementById("board");
 
-const board = document.createElement("div");
-board.classList.add("board");
-board.textContent = "whattttt";
 
-container.appendChild(board);
+document.addEventListener("DOMContentLoaded", function() {
+    createBoard(16);
+    console.log("hi")
+});
+console.log("hey")
+
+function createBoard(size) {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+};
