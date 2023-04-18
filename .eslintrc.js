7f485d300673fd1f3537c7869f4d81cbd3bf913e
simca09 <delacruz.klydean@yahoo.com>
2023-04-18 10:11:17 -0400

    eslintrc added

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "extends": "prettier",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
